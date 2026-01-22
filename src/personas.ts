d9364537cf5b5cb8a65bb4bfea8713e157bfe020
Gabriel Vian <gvian07@gmail.com>
2026-01-22 09:24:13 -0800

    Revert "Add debug line"
    This reverts commit 33a7de0c310223d39ba677ba7a5e00eb016ae2e7.

--- src/personas.ts
+++ src/personas.ts
@@ -6,5 +6,4 @@ export const personas: Persona[] = [];
 
 personas.push(new Persona("Gabriel", "Student", "CSUSM", "1234567890", "vian003@csusm.edu"));
 
-console.log('Loaded', csv)
 personas.push(...CsvToPersonas(csv));

33a7de0c310223d39ba677ba7a5e00eb016ae2e7
Gabriel Vian <gvian07@gmail.com>
2026-01-22 09:23:27 -0800

    Add debug line

--- src/personas.ts
+++ src/personas.ts
@@ -6,4 +6,5 @@ export const personas: Persona[] = [];
 
 personas.push(new Persona("Gabriel", "Student", "CSUSM", "1234567890", "vian003@csusm.edu"));
 
+console.log('Loaded', csv)
 personas.push(...CsvToPersonas(csv));

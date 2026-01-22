b83430418deb80a1419d52a218aa58ab1b3208ab
Gabriel Vian <gvian07@gmail.com>
2026-01-21 17:47:36 -0800

    Add list page

---
+++ src/routes/list/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import {personas} from "../../personas";
+	import Persona from "../Persona.svelte";
+</script>
+
+{#each personas as persona}
+	<Persona {persona}/>
+
+	<br/>
+	<br/>
+{/each}

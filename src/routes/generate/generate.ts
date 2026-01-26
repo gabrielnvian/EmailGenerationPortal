6398c92dac71bf4a40e2eebb9b3854295946ed22
MarkAwad509 <markawad509@gmail.com>
2026-01-26 15:13:30 -0800

    started generate form action for n8n integration

--- src/routes/generate/generate.ts
+++ src/routes/generate/generate.ts
@@ -4,8 +4,15 @@
  * The n8n workflow is going to run on database trigger,
  * decreasing the number of emails to be generated at every iteration
  */
+import {Persona} from "../../personas.model";
 
-
-export function queueEmails(field: string, company: string, n: number) {
-
+export async function queueEmails(field: string, company: string, n: number) {
+  const url = "http://localhost:5678/webhook-test/ea07a640-6fbe-4334-939c-fec4c2eada89";
+  const reposnse = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      "name": ,
+      "company": company,
+    })
+  })
 }

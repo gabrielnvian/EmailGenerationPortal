112f466183c588405645bdbb844984bd41a5772b
Gabriel Vian <gvian07@gmail.com>
2026-01-26 12:36:27 -0800

    Add home button to list page

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import {personas} from "../../personas";
 	import Persona from "../Persona.svelte";
+	import {goto} from "$app/navigation";
+	import {base} from "$app/paths";
 </script>
+
+<button on:click={() => goto(`${base}/`)}>Home</button>
+<br/>
+<br/>
 
 {#each $personas as persona}
 	<Persona {persona}/>

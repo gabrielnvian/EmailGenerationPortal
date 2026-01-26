36f8769d0d6b8da704e6650136eb73da3bce95a0
Gabriel Vian <gvian07@gmail.com>
2026-01-26 15:10:44 -0800

    Add PersonaSelector.svelte

---
+++ src/PersonaSelector.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type {Persona} from "./personas.model";
+	import PersonaCard from "./routes/PersonaCard.svelte";
+	import type {Writable} from "svelte/store";
+
+	export let personas: Persona[] = [];
+	export let selectedPersonas: Writable<Persona[]>;
+
+	let personaIdx: number = 0;
+
+	function addPersona() {
+		const personaToBeAdded = personas[personaIdx];
+
+		if ($selectedPersonas.map(persona => persona.name).includes(personaToBeAdded.name)) return;
+
+		$selectedPersonas = [...$selectedPersonas, personaToBeAdded];
+	}
+</script>
+
+<select bind:value={personaIdx}>
+	{#each personas as persona, idx}
+		<option value={idx}>
+			{persona.name} at {persona.company}
+		</option>
+	{/each}
+</select>
+
+<button on:click={addPersona}>Add</button>
+
+<br/>
+
+{#each $selectedPersonas as persona}
+	<PersonaCard noSup {persona}/>
+
+	<br/>
+	<br/>
+{/each}

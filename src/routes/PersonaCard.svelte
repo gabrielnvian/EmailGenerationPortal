6da9049aa8720d075b079042141ef00170dcd123
Gabriel Vian <gvian07@gmail.com>
2026-01-26 15:31:29 -0800

    Improve styles in PersonaCard.svelte

--- src/routes/PersonaCard.svelte
+++ src/routes/PersonaCard.svelte
@@ -3,18 +3,23 @@
 
 	export let persona: Persona;
 	export let noSup: boolean = false;
+	export let isSup: boolean = false;
+
+	$: showSup = persona.supervisor && !noSup;
 </script>
 
-<strong>Name: {persona.name}</strong><br/>
-<span>Title: {persona.jobTitle}</span><br/>
-<span>Company: {persona.company}</span><br/>
-<span>Field: {persona.field}</span><br/>
-<span>Phone: {persona.phone}</span><br/>
-<span>Email: {persona.email}</span><br/>
+<div style="padding: 5px; border: {isSup ? '' : '1px dashed gray'}">
+	<strong>Name: {persona.name}</strong><br/>
+	<span>Title: {persona.jobTitle}</span><br/>
+	<span>Company: {persona.company}</span><br/>
+	<span>Field: {persona.field}</span><br/>
+	<span>Phone: {persona.phone}</span><br/>
+	<span>Email: {persona.email}</span><br/>
 
-{#if persona.supervisor && !noSup}
-	<span>Supervisor:</span>
-	<ul>
-		<svelte:self persona={persona.supervisor}/>
-	</ul>
-{/if}
+	{#if showSup}
+		<span>Supervisor:</span>
+		<ul style="margin: 0">
+			<svelte:self isSup persona={persona.supervisor}/>
+		</ul>
+	{/if}
+</div>

895d14cc87bf5e47f485b09b152fc87dc9ca43ca
Gabriel Vian <gvian07@gmail.com>
2026-01-26 12:53:22 -0800

    Create placeholder file for generate.ts

---
+++ src/routes/generate/generate.ts
@@ -0,0 +1,6 @@
+/**
+ * Set up an API with n8n so that we can create a database record containing
+ * the field and the company selected by the user, as well as the number of emails to be generated.
+ * The n8n workflow is going to run on database trigger,
+ * decreasing the number of emails to be generated at every iteration
+ */

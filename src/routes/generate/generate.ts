4944cd550e6b5f03a2ca5fc56dc50baa78ef6e55
Gabriel Vian <gvian07@gmail.com>
2026-01-26 22:18:53 -0800

    Change webhook to production URL

--- src/routes/generate/generate.ts
+++ src/routes/generate/generate.ts
@@ -1,26 +1,20 @@
-/**
- * Set up an API with n8n so that we can create a database record containing
- * the field and the company selected by the user, as well as the number of emails to be generated.
- * The n8n workflow is going to run on database trigger,
- * decreasing the number of emails to be generated at every iteration
- */
 import {Persona} from "../../personas.model";
 
 export async function queueEmails(fromField: string, toField: string, idea: string, fromPersonas: Persona[], toPersonas: Persona[], n: number) {
-  const url = "https://n8n.tail068f9.ts.net/webhook-test/generate-email";
+	const url = "https://n8n.tail068f9.ts.net/webhook/generate-email";
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      "fromField": fromField,
-      "toField": toField,
-      "idea": idea,
-      "from": fromPersonas.map(persona => JSON.stringify(persona)),
-      "to": toPersonas.map(persona => JSON.stringify(persona)),
-      "emailCount": n
-    })
-  });
+	const response = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			fromField,
+			toField,
+			idea,
+			from: fromPersonas.map(persona => JSON.stringify(persona)),
+			to: toPersonas.map(persona => JSON.stringify(persona)),
+			emailCount: n
+		})
+	});
 }
